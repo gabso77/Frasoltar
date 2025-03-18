180ddb021a28cc98f1a8fd1650701c9814fecff6
Marco <marcofratello2004@gmail.com>
2025-03-18 11:17:28 +0100

    PULIZIA

--- backend/urls.py
+++ backend/urls.py
@@ -3,12 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.pullman, name = 'home'),
-    path('contattaci', views.contattaci, name = 'contattaci'),
-    path('tratte', views.tratte, name = 'tratte'),
-    path('tratte/', views.tratta_view, name='tratta_view'),
-    path('conferma_prenotazione/<int:tratta_id>/', views.conferma_prenotazione, name='conferma_prenotazione'),
-    path('conferma_successo/<int:prenotazione_id>/', views.conferma_successo, name='conferma_successo'),
     path('lista_citta', views.lista_citta, name='lista_citta'),
     path('lista_tratte', views.lista_tratte, name='lista_tratte'),
     path('conferma_Prenotazione', views.conferma_Prenotazione, name='conferma_Prenotazione')
