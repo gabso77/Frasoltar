66d64c58c195ad888c279158e29cb24cb51d836c
gabso_7 <solitrogabriele2@gmail.com>
2025-07-01 12:33:42 +0200

    divisione script login in tutte le pagine

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -368,38 +368,6 @@ let loginForm = document.getElementById("loginForm");
 }});
 
 
-// UTENTE LOGGATO
-document.addEventListener("DOMContentLoaded", function () {
-    const loggedInUser = localStorage.getItem("loggedInUser");
-
-    const userInfoDiv = document.getElementById("user-info");
-    const usernameSpan = document.getElementById("username");
-    const signupBtn = document.getElementById("signup-btn");
-    const loginBtn = document.getElementById("login-btn");
-    const logoutBtn = document.getElementById("logout-btn");
-
-    if (loggedInUser) {
-        usernameSpan.textContent = loggedInUser;
-        userInfoDiv.style.display = "block";
-        signupBtn.style.display = "none";
-        loginBtn.style.display = "none";
-        logoutBtn.style.display = "inline-block";
-    } else {
-        logoutBtn.style.display = "none";
-    }
-
-
-    if (logoutBtn) {
-        logoutBtn.addEventListener("click", function (e) {
-            e.preventDefault();
-            localStorage.removeItem("loggedInUser"); // pulizia storage
-            window.location.href = "./pages/login.html"; // redirect diretto
-        });
-    }
-});
-
-
-
 
 
 
